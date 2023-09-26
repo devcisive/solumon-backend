219dc89e37126b02391adec456de6a26f1540ccf
GyeongSe99 <sgcheon@naver.com>
2023-09-26 21:58:58 +0900

    Test : 테스트 코드 추가, ErrorCode 및 Dto구성 변경

--- solumon-backend/src/main/java/com/example/solumonbackend/member/entity/Member.java
+++ solumon-backend/src/main/java/com/example/solumonbackend/member/entity/Member.java
@@ -14,11 +14,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
---
+++ solumon-backend/src/test/java/com/example/solumonbackend/member/service/MemberServiceTest.java
@@ -0,0 +1,266 @@
+package com.example.solumonbackend.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.solumonbackend.global.exception.ErrorCode;
+import com.example.solumonbackend.global.exception.MemberException;
+import com.example.solumonbackend.global.security.JwtTokenProvider;
+import com.example.solumonbackend.member.entity.Member;
+import com.example.solumonbackend.member.entity.RefreshToken;
+import com.example.solumonbackend.member.model.GeneralSignInDto;
+import com.example.solumonbackend.member.model.GeneralSignUpDto;
+import com.example.solumonbackend.member.model.GeneralSignUpDto.Response;
+import com.example.solumonbackend.member.repository.MemberRepository;
+import com.example.solumonbackend.member.repository.RefreshTokenRedisRepository;
+import com.example.solumonbackend.member.type.MemberRole;
+import java.util.Collections;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+  @Mock
+  private MemberRepository memberRepository;
+
+  @Mock
+  private PasswordEncoder passwordEncoder;
+
+  @Mock
+  private JwtTokenProvider jwtTokenProvider;
+
+  @Mock
+  private RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+  @InjectMocks
+  private MemberService memberService;
+
+  @DisplayName("일반 회원가입 - 성공")
+  @Test
+  void signUpTest_Success() {
+    // Given
+    GeneralSignUpDto.Request request = GeneralSignUpDto.Request.builder()
+        .email("test@example.com")
+        .password("password123!")
+        .nickname("testUser").build();
+
+    Member mockMember = Member.builder()
+        .memberId(5L)
+        .kakaoId(null)
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .nickname("testUser")
+        .role(MemberRole.GENERAL)
+        .reportCount(0)
+        .isFirstLogIn(true)
+        .build();
+
+    when(memberRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+    when(memberRepository.findByNickname(anyString())).thenReturn(Optional.empty());
+    when(memberRepository.save(any(Member.class))).thenReturn(mockMember);
+
+    // When
+    Response response = memberService.signUp(request);
+
+    // Then
+    assertThat(response.getEmail()).isEqualTo(request.getEmail());
+    assertThat(response.getNickname()).isEqualTo(request.getNickname());
+
+    verify(memberRepository, times(1)).save(any(Member.class));
+    verify(passwordEncoder, times(2)).encode(any(String.class));
+  }
+
+  @DisplayName("일반 회원가입 실패 - 중복된 이메일")
+  @Test
+  void signUpTest_DuplicatedEmail() {
+    // Given
+    GeneralSignUpDto.Request request = GeneralSignUpDto.Request.builder()
+        .email("test@example.com")
+        .password("password123!")
+        .nickname("testUser").build();
+
+    when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(new Member()));
+
+    // when
+    MemberException exception = assertThrows(MemberException.class,
+        () -> memberService.signUp(request));
+
+    // then
+    verify(memberRepository, times(1)).findByEmail(request.getEmail());
+    assertEquals(ErrorCode.ALREADY_REGISTERED_EMAIL, exception.getErrorCode());
+  }
+
+  @DisplayName("일반 회원가입 실패 - 중복된 닉네임")
+  @Test
+  void signUpTest_DuplicatedNickName() {
+    // Given
+    GeneralSignUpDto.Request request = GeneralSignUpDto.Request.builder()
+        .email("test@example.com")
+        .password("password123!")
+        .nickname("testUser").build();
+
+    when(memberRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+    when(memberRepository.findByNickname(anyString())).thenReturn(Optional.of(new Member()));
+
+    // when
+    MemberException exception = assertThrows(MemberException.class,
+        () -> memberService.signUp(request));
+
+    // then
+    verify(memberRepository, times(1)).findByEmail(request.getEmail());
+    verify(memberRepository, times(1)).findByNickname(request.getNickname());
+    assertEquals(ErrorCode.ALREADY_REGISTERED_NICKNAME, exception.getErrorCode());
+  }
+
+  @DisplayName("일반 로그인 - 성공")
+  @Test
+  void signInTest_Success() {
+    // Given
+    String email = "test@example.com";
+    String password = "password123!";
+
+    GeneralSignInDto.Request request = GeneralSignInDto.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    Member mockMember = Member.builder()
+        .memberId(5L)
+        .kakaoId(null)
+        .email(email)
+        .password(passwordEncoder.encode(password))
+        .nickname("testUser")
+        .role(MemberRole.GENERAL)
+        .reportCount(0)
+        .isFirstLogIn(true)
+        .build();
+
+    when(memberRepository.findByEmail(email)).thenReturn(Optional.of(mockMember));
+    when(passwordEncoder.matches(password, mockMember.getPassword())).thenReturn(true);
+
+    // When
+    GeneralSignInDto.Response response = memberService.signIn(request);
+
+    // Then
+    verify(memberRepository, times(1)).findByEmail(email);
+    verify(passwordEncoder, times(1)).matches(password, mockMember.getPassword());
+    verify(jwtTokenProvider, times(1)).createAccessToken(email,
+        Collections.singletonList("ROLE_GENERAL"));
+    verify(jwtTokenProvider, times(1)).createRefreshToken(email,
+        Collections.singletonList("ROLE_GENERAL"));
+    verify(refreshTokenRedisRepository, times(1)).save(any(RefreshToken.class));
+
+    assertNotNull(response);
+    assertEquals(mockMember.getMemberId(), response.getMemberId());
+    assertEquals(mockMember.isFirstLogIn(), response.isFirstLogIn());
+  }
+
+  @DisplayName("일반 로그인 실패 - 아이디 불일치")
+  @Test
+  void signInTest_IncorrectEmail() {
+    // 저장 안되어있는 테스트 유저 정보
+    String email = "test@example.com";
+    String password = "password123!";
+
+    GeneralSignInDto.Request request = GeneralSignInDto.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    // when
+    MemberException exception = assertThrows(MemberException.class,
+        () -> memberService.signIn(request));
+
+    // then
+    verify(memberRepository, times(1)).findByEmail(email);
+
+    assertEquals(ErrorCode.NOT_FOUND_MEMBER, exception.getErrorCode());
+  }
+
+  @DisplayName("일반 로그인 실패 - 비밀번호 불일치")
+  @Test
+  void signInTest_IncorrectPassWord() {
+    // 저장 안되어있는 테스트 유저 정보
+    String email = "test@example.com";
+    String password = "password123!";
+
+    GeneralSignInDto.Request request = GeneralSignInDto.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    Member mockMember = Member.builder()
+        .memberId(5L)
+        .kakaoId(null)
+        .email(email)
+        .password(passwordEncoder.encode("password1456@!#$"))
+        .nickname("testUser")
+        .role(MemberRole.GENERAL)
+        .reportCount(0)
+        .isFirstLogIn(true)
+        .build();
+
+    when(memberRepository.findByEmail(email)).thenReturn(Optional.of(mockMember));
+
+    // when
+    MemberException exception = assertThrows(MemberException.class,
+        () -> memberService.signIn(request));
+
+    // then
+    verify(memberRepository, times(1)).findByEmail(email);
+    verify(passwordEncoder, times(1)).matches(password, mockMember.getPassword());
+
+    assertEquals(ErrorCode.NOT_CORRECT_PASSWORD, exception.getErrorCode());
+  }
+
+  @DisplayName("일반 로그인 실패 - 탈퇴한 유저 로그인 시도")
+  @Test
+  void signInTest_LoginWithDeletedUse() {
+    // 저장 안되어있는 테스트 유저 정보
+    String email = "test@example.com";
+    String password = "password123!";
+
+    GeneralSignInDto.Request request = GeneralSignInDto.Request.builder()
+        .email(email)
+        .password(password)
+        .build();
+
+    Member mockMember = Member.builder()
+        .memberId(5L)
+        .kakaoId(null)
+        .email(email)
+        .password(passwordEncoder.encode("password1456@!#$"))
+        .nickname("testUser")
+        .role(MemberRole.GENERAL)
+        .reportCount(0)
+        .isFirstLogIn(true)
+        .build();
+
+    when(memberRepository.findByEmail(email)).thenReturn(Optional.of(mockMember));
+
+    // when
+    MemberException exception = assertThrows(MemberException.class,
+        () -> memberService.signIn(request));
+
+    // then
+    verify(memberRepository, times(1)).findByEmail(email);
+    verify(passwordEncoder, times(1)).matches(password, mockMember.getPassword());
+
+    assertEquals(ErrorCode.NOT_CORRECT_PASSWORD, exception.getErrorCode());
+  }
+
+}
