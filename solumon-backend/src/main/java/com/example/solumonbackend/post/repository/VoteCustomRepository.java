c97dc973db576721c2bb28ca5ca8b3559d885979
Gayeon <922g@naver.com>
2023-09-27 00:01:18 +0900

    Refactor : query 작동이 되도록 수정함(queryDSL 공부 후 수정할 예정)

--- solumon-backend/src/main/java/com/example/solumonbackend/post/repository/VoteCustomRepository.java
+++ solumon-backend/src/main/java/com/example/solumonbackend/post/repository/VoteCustomRepository.java
@@ -1,12 +1,12 @@
 package com.example.solumonbackend.post.repository;
 
-import com.example.solumonbackend.post.entity.Post;
 import com.example.solumonbackend.post.model.PostDto.ChoiceResultDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class VoteCustomRepository {
@@ -14,16 +14,29 @@ public class VoteCustomRepository {
   @PersistenceContext
   private EntityManager em;
 
-  public List<ChoiceResultDto> getChoiceResults(Post post) {
-    return em.createQuery(
-            "SELECT NEW com.example.solumonbackend.post.model.PostDto.ChoiceResultDto(c.choiceNum, c.choiceText, " +
-                "COUNT(*), (COUNT(*) * 100.0 / SUM(COUNT(*)) OVER ())) "
+  public List<ChoiceResultDto> getChoiceResults(Long postId) {
+    // choicePercent 도 함께 쿼리에서 계산하고 싶었으나 계속되는 오류로 제외하고 구함(queryDSL 공부 후 수정 예정)
+    List<ChoiceResultDto> resultList = em.createQuery(
+            "SELECT new com.example.solumonbackend.post.model.PostDto$ChoiceResultDto" +
+                "(c.choiceNum, c.choiceText, COUNT(v.selectedNum), 0.0) "
                 + "FROM Choice c "
-                + "JOIN Vote v ON c.post = v.post AND c.choiceNum = v.selectedNum "
-                + "WHERE c.post = :post "
+                + "LEFT JOIN Vote v ON c.post.postId = v.post.postId AND c.choiceNum = v.selectedNum "
+                + "WHERE c.post.postId = :postId "
                 + "GROUP BY c.choiceNum, c.choiceText "
+                + "having COUNT(*) > 0 "
                 + "ORDER BY c.choiceNum", ChoiceResultDto.class)
-        .setParameter("post", post)
+        .setParameter("postId", postId)
         .getResultList();
+
+    long countSum = resultList.stream().mapToLong(ChoiceResultDto::getChoiceCount).sum();
+
+    return resultList.stream()
+        .map(result -> ChoiceResultDto.builder()
+            .choiceNum(result.getChoiceNum())
+            .choiceText(result.getChoiceText())
+            .choiceCount(result.getChoiceCount())
+            .choicePercent(countSum == 0 ? 0.0 : result.getChoiceCount() * 100.0 / countSum)
+            .build())
+        .collect(Collectors.toList());
   }
 }
