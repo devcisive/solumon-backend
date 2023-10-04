c375b5a21f72357b338c822ad5dcfffc95ecb883
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2023-10-04 13:33:30 +0900

    Test:  TestMyParticipatePostData 레이아웃 수정
    - 빌더의 형태를 하나씩 아래로 띄우는 방식으로 수정

--- solumon-backend/src/test/java/com/example/solumonbackend/member/model/TestMyParticipatePostData.java
+++ solumon-backend/src/test/java/com/example/solumonbackend/member/model/TestMyParticipatePostData.java
@@ -17,29 +17,58 @@ public class TestMyParticipatePostData {
     List<Post> myWritePosts = new ArrayList<>();
 
     myWritePosts.add(
-        Post.builder().member(fakeMember).title("myWritePost1").contents("최신5,마감,투표5,채팅1")
-            .createdAt(LocalDateTime.now().minusDays(5)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(10).chatCount(50).build());
+        Post.builder()
+            .member(fakeMember)
+            .title("myWritePost1")
+            .contents("최신5,마감,투표5,채팅1")
+            .createdAt(LocalDateTime.now().minusDays(5))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(10)
+            .chatCount(50)
+            .build());
 
     myWritePosts.add(
-        Post.builder().member(fakeMember).title("myWritePost2").contents("최신4,마감,투표4,채팅2")
-            .createdAt(LocalDateTime.now().minusDays(4)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(20).chatCount(40).build());
+        Post.builder()
+            .member(fakeMember)
+            .title("myWritePost2")
+            .contents("최신4,마감,투표4,채팅2")
+            .createdAt(LocalDateTime.now().minusDays(4))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(20)
+            .chatCount(40)
+            .build());
 
     myWritePosts.add(
-        Post.builder().member(fakeMember).title("myWritePost3").contents("최신3,마감,투표3,채팅3")
-            .createdAt(LocalDateTime.now().minusDays(3)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(30).chatCount(30).build());
+        Post.builder()
+            .member(fakeMember)
+            .title("myWritePost3")
+            .contents("최신3,마감,투표3,채팅3")
+            .createdAt(LocalDateTime.now().minusDays(3))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(30)
+            .chatCount(30)
+            .build());
 
     myWritePosts.add(
-        Post.builder().member(fakeMember).title("myWritePost4").contents("최신2,진행,투표2,채팅4")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(40).chatCount(20).build());
+        Post.builder().member(fakeMember)
+            .title("myWritePost4")
+            .contents("최신2,진행,투표2,채팅4")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(40)
+            .chatCount(20)
+            .build());
 
     myWritePosts.add(
-        Post.builder().member(fakeMember).title("myWritePost5").contents("최신1,진행,투표1,채팅5")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(50).chatCount(10).build());
+        Post.builder()
+            .member(fakeMember)
+            .title("myWritePost5")
+            .contents("최신1,진행,투표1,채팅5")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(50)
+            .chatCount(10)
+            .build());
 
     return myWritePosts;
   }
@@ -49,29 +78,58 @@ public class TestMyParticipatePostData {
     List<Vote> myVotes;
 
     myVotePosts.add(
-        Post.builder().member(otherMember).title("myVotePost1").contents("최신5,마감,투표5,채팅1")
-            .createdAt(LocalDateTime.now().minusDays(5)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(10).chatCount(50).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myVotePost1")
+            .contents("최신5,마감,투표5,채팅1")
+            .createdAt(LocalDateTime.now().minusDays(5))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(10)
+            .chatCount(50)
+            .build());
 
     myVotePosts.add(
-        Post.builder().member(otherMember).title("myVotePost2").contents("최신4,마감,투표4,채팅2")
-            .createdAt(LocalDateTime.now().minusDays(4)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(20).chatCount(40).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myVotePost2")
+            .contents("최신4,마감,투표4,채팅2")
+            .createdAt(LocalDateTime.now().minusDays(4))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(20)
+            .chatCount(40)
+            .build());
 
     myVotePosts.add(
-        Post.builder().member(otherMember).title("myVotePost3").contents("최신3,마감,투표3,채팅3")
-            .createdAt(LocalDateTime.now().minusDays(3)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(30).chatCount(30).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myVotePost3")
+            .contents("최신3,마감,투표3,채팅3")
+            .createdAt(LocalDateTime.now().minusDays(3))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(30)
+            .chatCount(30)
+            .build());
 
     myVotePosts.add(
-        Post.builder().member(otherMember).title("myVotePost4").contents("최신2,진행,투표2,채팅4")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(40).chatCount(20).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myVotePost4")
+            .contents("최신2,진행,투표2,채팅4")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(40)
+            .chatCount(20)
+            .build());
 
     myVotePosts.add(
-        Post.builder().member(otherMember).title("myVotePost5").contents("최신1,진행,투표1,채팅5")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(50).chatCount(10).build());
+        Post.builder().member(otherMember)
+            .title("myVotePost5")
+            .contents("최신1,진행,투표1,채팅5")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(50)
+            .chatCount(10)
+            .build());
 
     myVotes = myVotePosts.stream()
         .map(post -> Vote.builder().post(post).member(fakeMember).build())
@@ -86,29 +144,57 @@ public class TestMyParticipatePostData {
     List<ChannelMember> myChats;
 
     myChatPosts.add(
-        Post.builder().member(otherMember).title("myChatPost1").contents("최신5,마감,투표5,채팅1")
-            .createdAt(LocalDateTime.now().minusDays(5)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(10).chatCount(50).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myChatPost1")
+            .contents("최신5,마감,투표5,채팅1")
+            .createdAt(LocalDateTime.now().minusDays(5))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(10)
+            .chatCount(50)
+            .build());
 
     myChatPosts.add(
-        Post.builder().member(otherMember).title("myChatPost2").contents("최신4,마감,투표4,채팅2")
-            .createdAt(LocalDateTime.now().minusDays(4)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(20).chatCount(40).build());
+        Post.builder()
+            .member(otherMember).
+            title("myChatPost2").contents("최신4,마감,투표4,채팅2")
+            .createdAt(LocalDateTime.now().minusDays(4))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(20)
+            .chatCount(40)
+            .build());
 
     myChatPosts.add(
-        Post.builder().member(otherMember).title("myChatPost3").contents("최신3,마감,투표3,채팅3")
-            .createdAt(LocalDateTime.now().minusDays(3)).endAt(LocalDateTime.now().plusDays(1))
-            .voteCount(30).chatCount(30).build());
+        Post.builder().member(otherMember)
+            .title("myChatPost3")
+            .contents("최신3,마감,투표3,채팅3")
+            .createdAt(LocalDateTime.now().minusDays(3))
+            .endAt(LocalDateTime.now().plusDays(1))
+            .voteCount(30)
+            .chatCount(30)
+            .build());
 
     myChatPosts.add(
-        Post.builder().member(otherMember).title("myChatPost4").contents("최신2,진행,투표2,채팅4")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(40).chatCount(20).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myChatPost4")
+            .contents("최신2,진행,투표2,채팅4")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(40)
+            .chatCount(20)
+            .build());
 
     myChatPosts.add(
-        Post.builder().member(otherMember).title("myChatPost5").contents("최신1,진행,투표1,채팅5")
-            .createdAt(LocalDateTime.now().minusDays(1)).endAt(LocalDateTime.now().minusDays(1))
-            .voteCount(50).chatCount(10).build());
+        Post.builder()
+            .member(otherMember)
+            .title("myChatPost5")
+            .contents("최신1,진행,투표1,채팅5")
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .endAt(LocalDateTime.now().minusDays(1))
+            .voteCount(50)
+            .chatCount(10)
+            .build());
 
     myChats = myChatPosts.stream()
         .map(post -> ChannelMember.builder().post(post).member(fakeMember).build())
