aca78a4c25439cfebc21dac4897bd4f8b8f346c6
GyeongSe99 <sgcheon@naver.com>
2023-10-09 16:56:38 +0900

    Refactoer: 메소드 이름 변경

--- solumon-backend/src/main/java/com/example/solumonbackend/post/model/PageRequestCustom.java
+++ solumon-backend/src/main/java/com/example/solumonbackend/post/model/PageRequestCustom.java
@@ -14,10 +14,9 @@ Pageable 의 size값의 limit가 없는 문제, page가 0부터 시작하는 점
 
   private static final int size = 10;  // 페이지당 가져올 데이터 수
 
-
   public static PageRequest of(int page, PostOrder postOrder) {
     page = page <= 0 ? 1 : page;
-    return PageRequest.of(page-1, size, Sort.by(Sort.Direction.DESC, postOrder.getOrderTarget()));
+    return PageRequest.of(page-1, size, Sort.by(Sort.Direction.DESC, postOrder.getSortCriteria()));
     // 페이지가 1부터 시작하는 것을 조정 (1페이지를 보고싶을땐 실제로 0페이지에 해당하는 데이터 넘겨야함
   }
 }
