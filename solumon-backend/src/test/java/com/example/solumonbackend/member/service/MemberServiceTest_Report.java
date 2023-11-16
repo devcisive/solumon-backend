5228b4950a8562870d8dff90ba2429325dc8909f
DokyungYou <125132565+DokyungYou@users.noreply.github.com>
2023-11-16 17:48:03 +0900

    Test: 신고관련기능 수정
    - 신고주제 추가(게시글, 채팅으로 분리)
    - 게시글의 신고상태유무 추가(신고상태일 경우 수정, 삭제 불가)

--- solumon-backend/src/test/java/com/example/solumonbackend/member/service/MemberServiceTest_Report.java
+++ solumon-backend/src/test/java/com/example/solumonbackend/member/service/MemberServiceTest_Report.java
@@ -1,12 +1,13 @@
 package com.example.solumonbackend.member.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.example.solumonbackend.chat.repository.ChatMessageRepository;
 import com.example.solumonbackend.global.exception.ErrorCode;
 import com.example.solumonbackend.global.exception.MemberException;
 import com.example.solumonbackend.member.entity.Member;
@@ -17,7 +18,11 @@ import com.example.solumonbackend.member.model.ReportDto.Request;
 import com.example.solumonbackend.member.repository.MemberRepository;
 import com.example.solumonbackend.member.repository.ReportRepository;
 import com.example.solumonbackend.member.type.MemberRole;
+import com.example.solumonbackend.member.type.ReportSubject;
 import com.example.solumonbackend.member.type.ReportType;
+import com.example.solumonbackend.post.entity.Post;
+import com.example.solumonbackend.post.repository.PostRepository;
+import com.example.solumonbackend.post.type.PostStatus;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,6 +43,12 @@ class MemberServiceTest_Report {
   @Mock
   private ReportRepository reportRepository;
 
+  @Mock
+  private PostRepository postRepository;
+
+  @Mock
+  private ChatMessageRepository chatMessageRepository;
+
 
   @InjectMocks
   private MemberService memberService;
@@ -50,6 +61,7 @@ class MemberServiceTest_Report {
 //  private ReportDto.Request request;
 
   private Report report;
+
 
   @BeforeEach
   public void dataSetup() {
@@ -75,22 +87,38 @@ class MemberServiceTest_Report {
 
     report = Report.builder()
         .reportId(1L)
+        .reporterId(reporterMember.getMemberId())
         .reportedAt(LocalDateTime.now())
+        .reportSubject(ReportSubject.POST)
         .reportType(ReportType.OTHER)
-        .content("신고합니다")
+        .reportExplanation("신고합니다")
         .build();
 
   }
 
 
-  @DisplayName("유저신고 - 성공( 내가 해당유저를 신고한적은 있으나 3일이 지남)")
+  @DisplayName("게시글신고 - 성공( 내가 해당유저를 신고한적은 있으나 3일이 지남)")
   @Test
   void reportMember_success_reportablePeriod() {
 
     // Given
     Member otherMember = otherMemberBuilder.build();
-    ReportDto.Request request = new Request(otherMember.getMemberId(),ReportType.OTHER,"분란을 조장해요");
 
+    Post post = Post.builder()
+        .postId(1L)
+        .member(otherMember)
+        .build();
+
+
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(otherMember.getMemberId())
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
+
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
     when(memberRepository.findById(request.getReportedMemberId())).thenReturn(Optional.of(otherMember));
     when(reportRepository.findTopByMemberMemberIdAndReporterIdOrderByReportedAtDesc(
         otherMember.getMemberId(), reporterMember.getMemberId()))
@@ -100,16 +128,29 @@ class MemberServiceTest_Report {
             .reporterId(reporterMember.getMemberId())
             .reportedAt(LocalDateTime.now().minusDays(4)) // 가장 최근의 신고날짜가 4일전
             .build()));
+
+    when(postRepository.save(post)).thenReturn(post);
     when(reportRepository.save(any())).thenReturn(report);
 
-    ArgumentCaptor<Report> memberArgumentCaptor = ArgumentCaptor.forClass(Report.class);
+    ArgumentCaptor<Report> reportArgumentCaptor = ArgumentCaptor.forClass(Report.class);
+    ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
 
     // When
     memberService.reportMember(reporterMember, request);
 
     // Then
     verify(memberRepository, times(1)).findById(request.getReportedMemberId());
-    verify(reportRepository, times(1)).save(memberArgumentCaptor.capture());
+    verify(postRepository,times(1)).findById(request.getPostId());
+    verify(postRepository,times(1)).save(postArgumentCaptor.capture());
+    verify(reportRepository, times(1)).save(reportArgumentCaptor.capture());
+
+    Post captorPost = postArgumentCaptor.getValue();
+    Report captorReport = reportArgumentCaptor.getValue();
+
+    assertEquals(post.getPostId(),captorPost.getPostId());
+    assertEquals(PostStatus.REPORTED, captorPost.getPostStatus());
+    assertEquals(post.getPostId(),captorReport.getPostId());
+
   }
 
 
@@ -119,22 +160,50 @@ class MemberServiceTest_Report {
 
     // Given
     Member otherMember = otherMemberBuilder.build();
-    ReportDto.Request request = new Request(otherMember.getMemberId(),ReportType.OTHER,"분란을 조장해요");
+
+    Post post = Post.builder()
+        .postId(1L)
+        .member(otherMember)
+        .build();
+
+
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(otherMember.getMemberId())
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
 
     when(memberRepository.findById(request.getReportedMemberId())).thenReturn(Optional.of(otherMember));
     when(reportRepository.findTopByMemberMemberIdAndReporterIdOrderByReportedAtDesc(
         otherMember.getMemberId(), reporterMember.getMemberId()))
         .thenReturn(Optional.empty()); // 신고한 적 없음
 
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
+
+    post.reportPost();
+    when(postRepository.save(post)).thenReturn(post);
     when(reportRepository.save(any())).thenReturn(report);
-    ArgumentCaptor<Report> memberArgumentCaptor = ArgumentCaptor.forClass(Report.class);
+
+    ArgumentCaptor<Report> reportArgumentCaptor = ArgumentCaptor.forClass(Report.class);
+    ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
 
     // When
     memberService.reportMember(reporterMember, request);
 
     // Then
     verify(memberRepository, times(1)).findById(request.getReportedMemberId());
-    verify(reportRepository, times(1)).save(memberArgumentCaptor.capture());
+    verify(postRepository,times(1)).findById(request.getPostId());
+    verify(postRepository,times(1)).save(postArgumentCaptor.capture());
+    verify(reportRepository, times(1)).save(reportArgumentCaptor.capture());
+
+    Post captorPost = postArgumentCaptor.getValue();
+    Report captorReport = reportArgumentCaptor.getValue();
+
+    assertEquals(post.getPostId(),captorPost.getPostId());
+    assertEquals(PostStatus.REPORTED, captorPost.getPostStatus());
+    assertEquals(post.getPostId(),captorReport.getPostId());
   }
 
   @DisplayName("유저신고 - 실패(존재하지 않는 유저)")
@@ -142,8 +211,19 @@ class MemberServiceTest_Report {
   void reportMember_fail_NOT_FOUND_MEMBER() {
 
     // Given
-    ReportDto.Request request = new Request(-1L ,ReportType.OTHER,"분란을 조장해요");
+    Post post = Post.builder()
+        .postId(1L)
+        .build();
 
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(-1L)
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
+
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
     when(memberRepository.findById(-1L)).thenReturn(Optional.empty());
 
     // When
@@ -163,8 +243,20 @@ class MemberServiceTest_Report {
 
     // Given
     Member otherMember = otherMemberBuilder.unregisteredAt(LocalDateTime.now()).build();
-    ReportDto.Request request = new Request(otherMember.getMemberId(),ReportType.OTHER,"분란을 조장해요");
 
+    Post post = Post.builder()
+        .postId(1L)
+        .build();
+
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(-1L)
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
+
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
     when(memberRepository.findById(request.getReportedMemberId())).thenReturn(Optional.of(otherMember)); // 탈퇴한 멤버
 
     // When
@@ -185,8 +277,20 @@ class MemberServiceTest_Report {
 
     // Given
     Member otherMember = otherMemberBuilder.bannedAt(LocalDateTime.now()).build();
-    ReportDto.Request request = new Request(otherMember.getMemberId(),ReportType.OTHER,"분란을 조장해요");
 
+    Post post = Post.builder()
+        .postId(1L)
+        .build();
+
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(-1L)
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
+
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
     when(memberRepository.findById(request.getReportedMemberId())).thenReturn(Optional.of(otherMember));
 
     // When
@@ -206,8 +310,20 @@ class MemberServiceTest_Report {
   void reportMember_fail_COOL_TIME_REPORT_MEMBER() {
     // Given
     Member otherMember = otherMemberBuilder.build();
-    ReportDto.Request request = new Request(otherMember.getMemberId(),ReportType.OTHER,"분란을 조장해요");
 
+    Post post = Post.builder()
+        .postId(1L)
+        .build();
+
+    ReportDto.Request request = Request.builder()
+        .reportedMemberId(-1L)
+        .postId(post.getPostId())
+        .reportSubject(ReportSubject.POST)
+        .reportType(ReportType.OTHER)
+        .reportExplanation("신고합니다")
+        .build();
+
+    when(postRepository.findById(request.getPostId())).thenReturn(Optional.of(post));
     when(memberRepository.findById(request.getReportedMemberId())).thenReturn(Optional.of(otherMember));
     when(reportRepository.findTopByMemberMemberIdAndReporterIdOrderByReportedAtDesc(
         otherMember.getMemberId(), reporterMember.getMemberId()))
@@ -229,62 +345,62 @@ class MemberServiceTest_Report {
   }
 
 
-  @DisplayName("유저 정지 - 영구정지")
-  @Test
-  void banMember_PERMANENT_BAN() {
-
-    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(15);
-    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
-    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
-
-    // When
-    memberService.banMember(reporterMember);
-
-    // Then
-    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
-    Member captorValue = memberArgumentCaptor.getValue();
-
-    assertEquals(MemberRole.PERMANENT_BAN.value(), captorValue.getRole().value());
-
-  }
-
-  @DisplayName("유저 정지 - 정지")
-  @Test
-  void banMember_BAN() {
-
-    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(5);
-    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
-    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
-
-    // When
-    memberService.banMember(reporterMember);
-
-    // Then
-    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
-    Member captorValue = memberArgumentCaptor.getValue();
-
-    assertEquals(MemberRole.BANNED.value(), captorValue.getRole().value());
-
-  }
-
-
-  @DisplayName("유저 정지 - 정지조건 미충족")
-  @Test
-  void banMember_fail() {
-
-    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(7);
-    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
-    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
-
-    // When
-    memberService.banMember(reporterMember);
-
-    // Then
-    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
-    Member captorValue = memberArgumentCaptor.getValue();
-
-    assertEquals(MemberRole.GENERAL.value(), captorValue.getRole().value());
-
-  }
-
+//  @DisplayName("유저 정지 - 영구정지")
+//  @Test
+//  void banMember_PERMANENT_BAN() {
+//
+//    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(15);
+//    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
+//    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
+//
+//    // When
+//    memberService.banMember(reporterMember);
+//
+//    // Then
+//    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
+//    Member captorValue = memberArgumentCaptor.getValue();
+//
+//    assertEquals(MemberRole.PERMANENT_BAN.value(), captorValue.getRole().value());
+//
+//  }
+//
+//  @DisplayName("유저 정지 - 정지")
+//  @Test
+//  void banMember_BAN() {
+//
+//    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(5);
+//    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
+//    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
+//
+//    // When
+//    memberService.banMember(reporterMember);
+//
+//    // Then
+//    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
+//    Member captorValue = memberArgumentCaptor.getValue();
+//
+//    assertEquals(MemberRole.BANNED.value(), captorValue.getRole().value());
+//
+//  }
+//
+//
+//  @DisplayName("유저 정지 - 정지조건 미충족")
+//  @Test
+//  void banMember_fail() {
+//
+//    when(reportRepository.countByMember_MemberId(reporterMember.getMemberId())).thenReturn(7);
+//    when(memberRepository.save(reporterMember)).thenReturn(reporterMember);
+//    ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
+//
+//    // When
+//    memberService.banMember(reporterMember);
+//
+//    // Then
+//    verify(memberRepository, times(1)).save(memberArgumentCaptor.capture());
+//    Member captorValue = memberArgumentCaptor.getValue();
+//
+//    assertEquals(MemberRole.GENERAL.value(), captorValue.getRole().value());
+//
+//  }
+//
 }
